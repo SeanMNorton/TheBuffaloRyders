cf17dd6af16909119573aaf56957cdef1ace25bd
Sean <nortonseanm@gmail.com>
2019-04-10 11:21:00 -0400

    plan, clean, setup

---
+++ src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="nav">
+      <ul>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/music">Music</router-link>
+      </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+    name: 'Navigation',
+    props: {
+        links: Array,
+    },
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ 
+</style>
---
+++ src/views/Music.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="Music">
+    <h1>This is a Music page</h1>
+  </div>
+</template>
