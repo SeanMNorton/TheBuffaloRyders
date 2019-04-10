dfae9b93f021719f5340880a506afac0e19eb948
Sean <nortonseanm@gmail.com>
2019-04-10 16:27:40 -0400

    create footer component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
   <div id="app">
     <Header/>
     <router-view/>
-    <p>Footer Component</p>
+    <Footer/>
   </div>
 </template>
 
 
 <script>
 import Header from '@/components/Header/Header.vue';
+import Footer from '@/components/Footer/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer">
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+
+<style scoped lang="scss">
+    @import '../../assets/variables.scss';
+    .footer {
+      width: 100vw;
+      position: absolute;
+      bottom: 0;
+      min-height: 50px;
+      background-color: lighten($color-black, 10%);
+    }
+</style>
