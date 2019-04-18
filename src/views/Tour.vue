23e58519efbb133aadeec26abd1db664d35cda94
Sean <nortonseanm@gmail.com>
2019-04-18 19:48:33 -0400

    add gigs meta data

--- src/views/Tour.vue
+++ src/views/Tour.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="tour">
+    <MetaData v-if="metaDatas" :data="metaDatas[0]"/>
     <GigList :isNew="true" :limit='25'/>
     <GigList :isNew="false" :limit='25'/>
   </div>
@@ -8,13 +9,21 @@
 
 <script>
 import GigList from '@/components/Tour/GigList.vue';
+import { GET_METADATA } from '../queries';
+import MetaData from '@/components/UI/MetaData/MetaData.vue';
 
 export default {
   name: 'tour',
   components: {
     GigList,
+    MetaData,
   },
-  methods: {
+  apollo: {
+    $loadingKey: 'loading',
+    metaDatas: {
+      query: GET_METADATA,
+      variables: { where: { AND: [{ page: 'gigs' }] } },
+    },
   },
 };
 </script>
