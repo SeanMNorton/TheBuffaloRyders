325455a453e4c8299c7d55fb035d67d7fd2a6fcf
Sean <nortonseanm@gmail.com>
2019-04-18 11:04:13 -0400

    add music modal, mobile EPS

---
+++ src/components/Albums/AlbumModalBody.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="modal-body">
+    <div id="album-box" class="album-box">
+       <img v-if="album.albumCover" :src="album.albumCover.url" :alt="album.albumCover.filename">
+    </div>
+    <div class="album-info">
+      <p class="album-title">{{album.title}}  ({{getYear(album.releaseDate)}})</p>
+      <div class="song-list">
+        <div :key="i" v-for="(song, i) in album.songs"> {{song.trackNumber}}. {{song.title}} </div>
+      </div>
+
+      <div class="text">
+        <p>{{album.info}}</p>
+      </div>
+    </div>
+    <div class="brand-links">
+      <BrandLink name="spotify" :url="album.spotify"/>
+      <BrandLink name="itunes" :url="album.itunes"/>
+      <BrandLink name="bandcamp" :url="album.bandcamp"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import BrandLink from '../UI/BrandLink/BrandLink.vue';
+
+export default {
+  name: 'AlbumModalBody',
+  components: {
+    BrandLink,
+  },
+  props: ['album'],
+  computed: {
+    albumYear() {
+      return this.album.relaseDate;
+    },
+  },
+  methods: {
+    getYear: date => `${new Date(date).getFullYear()}`,
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+  @import '@/assets/variables.scss';
+  .modal-body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0px;
+    @include fluid-type($min-width, $max-width, $min-sub-header-font, $max-sub-header-font );
+  }
+  .album-box {
+    width: 200px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 200px;
+      height: 200px;
+    }
+  }
+  .album-title {
+    text-align: center;
+    margin-bottom: 3vh;
+    @include fluid-type($min-width, $max-width, $min-header-font, $max-header-font );
+
+  }
+  .song-list, .text {
+    font-family: $secondary-font;
+    padding: 0 10px;
+  }
+  .text {
+    margin-top: 1.5vh;
+    font-size: 0.6em;
+  }
+  .brand-links {
+    margin-top: 10px;
+    &>a {
+      padding: 0 10px;
+    }
+  }
+  @media( min-width: $break-point) {
+    .modal-body {
+      flex-direction: row;
+      justify-content: flex-start;
+    }
+  }
+</style>
