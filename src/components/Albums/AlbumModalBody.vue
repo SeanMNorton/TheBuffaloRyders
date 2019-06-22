19a5d0563a2245f9c38654fbc1edc36807cbe2ec
Sean <nortonseanm@gmail.com>
2019-06-22 15:07:45 -0400

    embeded spotify player to album modal

--- src/components/Albums/AlbumModalBody.vue
+++ src/components/Albums/AlbumModalBody.vue
@@ -1,26 +1,36 @@
 <template>
   <div class="modal-body">
-    <div id="album-box" class="album-box">
-       <img v-if="album.albumCover"
-        :src="'https://media.graphcms.com/resize=width:250/compress/'+album.albumCover.handle"
-        :alt="album.title + ' Album art.'"
-      >
+    <div class="main-info">
+
+      <div id="album-box" class="album-box">
+        <img v-if="album.albumCover"
+          :src="'https://media.graphcms.com/resize=width:250/compress/'+album.albumCover.handle"
+          :alt="album.title + ' Album art.'"
+        >
+      </div>
+      
+      <div class="album-info">
+        <p class="album-title">{{album.title}}  ({{getYear(album.releaseDate)}})</p>
+        <div class="song-list">
+          <div :key="i" v-for="(song, i) in album.songs"> {{song.trackNumber}}. {{song.title}} </div>
+        </div>
+
+        <div class="text">
+          <p>{{album.info}}</p>
+        </div>
+
+        <div class="brand-links">
+          <BrandLink name="spotify" :url="album.spotify"/>
+          <BrandLink name="itunes" :url="album.itunes"/>
+          <BrandLink name="bandcamp" :url="album.bandcamp"/>
+        </div>
+      </div>
+
     </div>
-    <div class="album-info">
-      <p class="album-title">{{album.title}}  ({{getYear(album.releaseDate)}})</p>
-      <div class="song-list">
-        <div :key="i" v-for="(song, i) in album.songs"> {{song.trackNumber}}. {{song.title}} </div>
-      </div>
 
-      <div class="text">
-        <p>{{album.info}}</p>
-      </div>
-
-      <div class="brand-links">
-        <BrandLink name="spotify" :url="album.spotify"/>
-        <BrandLink name="itunes" :url="album.itunes"/>
-        <BrandLink name="bandcamp" :url="album.bandcamp"/>
-      </div>
+    <div v-if="album.spotify" class="spotify-embed">
+      <Spinner/>
+      <iframe :src="spotifyEmbededUrl" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
     </div>
 
   </div>
@@ -28,17 +38,24 @@
 
 <script>
 import BrandLink from '../UI/BrandLink/BrandLink.vue';
+import Spinner from '../UI/Spinner/Spinner.vue';
 
 export default {
   name: 'AlbumModalBody',
   components: {
     BrandLink,
+    Spinner
   },
   props: ['album'],
   computed: {
     albumYear() {
       return this.album.relaseDate;
     },
+    
+    spotifyEmbededUrl() {
+      var albumUrl = this.album.spotify.match(/\/album(.*)$/g)[0]
+      return 'https://open.spotify.com/embed' + albumUrl
+    }
   },
   methods: {
     getYear: date => `${new Date(date).getFullYear()}`,
@@ -49,11 +66,12 @@ export default {
 
 <style lang="scss" scoped>
   @import '@/assets/variables.scss';
-  .modal-body {
+  .modal-body, .main-info {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    flex-wrap: wrap;
     margin: 0px;
     @include fluid-type($min-width, $max-width, $min-sub-header-font, $max-sub-header-font );
   }
@@ -94,20 +112,34 @@ export default {
       padding: 0 10px;
     }
   }
-  @media( min-width: 650px) {
-    .modal-body {
+  .spotify-embed {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 15px;
+    .fulfilling-bouncing-circle-spinner {
+      position: absolute;
+    }
+    iframe {
+      width: 70vw;
+      min-width: 300px;
+      z-index: 1;
+    }
+  }
+  
+  @media(min-width: 650px) {
+    .modal-body, .main-info{
       flex-direction: row;
-      justify-content: flex-start;
     }
     .album-info {
       display: flex;
       flex-direction: column;
+      max-width: 450px;
       padding-left: 40px;
     }
     .album-title {
       @include fluid-type($min-width, $max-width, $min-sub-header-font, $max-sub-header-font );
     }
-
     .album-box {
       width: 280px;
       height: 280px;
@@ -115,6 +147,24 @@ export default {
       img {
         width: 280px;
         height: 280px;
+      }
+    }
+  }
+
+   @media(min-width: 900px) {
+    .modal-body, .main-info{
+      justify-content: center;
+    }
+    .main-info {
+      flex-wrap: nowrap
+    }
+  }
+
+  @media(min-width: 1200px) {
+    .spotify-embed {
+      margin-left: 15px;
+      iframe {
+        width: 300px;
       }
     }
   }
