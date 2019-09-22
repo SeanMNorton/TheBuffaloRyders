e3d414589efb4bd3ee7223cfe1f74865fece1d38
Sean <nortonseanm@gmail.com>
2019-09-22 13:28:34 -0400

    fix store, refresh on navigation

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -3,21 +3,19 @@
     <div class="header">
       <h1>BUFFA-STORE</h1>
     </div>
+      
+    <div id="my-store-17654854"></div>
+    <script type="application/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-17654854");</script>
 
-    <div class="ec-cart-widget"></div>
-    <script type="application/javascript">Ecwid.init();</script>
-    <script type="applicaiton/javascript"> xProductBrowser("categoriesPerRow=3","views=grid(20,3) list(60) table(60)","categoryView=grid","searchView=list","id=my-store-17654854");</script>
-    <div id="my-store-17654854" style="max-width: 80vw; margin: 0 auto;"></div>
   </div>
-
 </template>
 
 
 <script>
   export default {
     name: 'store',
-    mounted() {
-
+    beforeCreate() {
+      // location.reload(true)
     }
   };
 </script>
