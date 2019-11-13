4ebe1978664a3da974ee153415211d1e4e2c5cec
Sean <nortonseanm@gmail.com>
2019-11-13 14:11:52 -0500

    add metadata to store page

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="container">
+    <MetaData v-if="metaDatas" :data="metaDatas[0]"/>
     <h1 class="header" style="text-align: center;">Merch</h1>
     <div id="my-store-17654854" style="max-width: 80vw; margin: auto;"></div>
   </div>
 </template>
 
 <script>
+import { GET_METADATA } from '../queries';
+import MetaData from '@/components/UI/MetaData/MetaData.vue';
   export default {
     name: 'store',
+    components: {
+      MetaData,
+    },
+    apollo: {
+      $loadingKey: 'loading',
+      metaDatas: {
+        query: GET_METADATA,
+        variables: { where: { page: 'store' } },
+      },
+    },
     beforeCreate() {
       this.$loadScript("https://app.ecwid.com/script.js?17654854&data_platform=code&data_date=2019-09-22")
       .then(() => {
